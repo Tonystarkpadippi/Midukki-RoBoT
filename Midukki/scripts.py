ebff3f42e3f3272f4e3f4ab880e8cde2d35dc7fc
Parthasaradhi <91871150+Tonystarkpadippi@users.noreply.github.com>
2022-11-27 13:55:17 +0530

    Update scripts.py

--- Midukki/scripts.py
+++ Midukki/scripts.py
@@ -5,7 +5,7 @@ I'm <b>{bot}</b> ✨ an advanced telegram Group management
 
 I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential..!
 
-Join <b><a href=http://t.me/Mo_Tech_YT>my news channel</a></b> to get information on all the latest updates
+Join <b><a href=https://t.me/PKB_BOTZ_UPDATES>BOT'S UPDATE CHANNEL</a></b> to get information on all the latest updates
 """
 
 HELP_TXT = """
@@ -27,7 +27,7 @@ ABOUT_TXT = """
 [{name}](t.me/{username}) Was created on September 4, 2022
 We are currently developing this bot, using only the Pyrogram library.
 
-➾ Developers : Muhammed
+➾ Developers : ᴍᴏᴏɴᴋɴɪɢʜᴛ
 ➾ Language : Python3
 ➾ Framework : Pyrogram
 ➾ Database : Mongo db
@@ -36,9 +36,8 @@ We are currently developing this bot, using only the Pyrogram library.
 DONATE_TXT = """
 If you like this project of mine, you can donate by clicking on the given link
 
-Dev : [Muhammed](t.me/Pr0fess0r_99)
-Paytm : [Click Here](https://p.paytm.me/xCTH/7yzmtgie)
-  or UPI `motech8129@okaxis`
+Dev : [ᴍᴏᴏɴᴋɴɪɢʜᴛ](https://t.me/Spidey_online)
+Update's channel : [PKB BOT'S UPDATES](https://t.me/PKB_BOTZ_UPDATES) 
 """
 
 STATUS_TXT = """
